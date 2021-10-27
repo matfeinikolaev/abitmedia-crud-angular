216c8d7ed23660e2142e8ae4ec8deeb29e8e4a5c
matfei_nikolaev <matfeinikolaev@gmail.com>
2021-10-27 11:56:35 -0500

    Added functionality for adding and editing an image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from '../services/product.service.ts.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
@@ -18,7 +19,11 @@ export class HomeComponent implements OnInit {
   @ViewChild("descriptionElement") descriptionElement: any;
   @ViewChild("priceElement") priceElement: any;
   @ViewChild("quantityElement") quantityElement: any;
-  constructor(private productService: ProductService) { }
+  @ViewChild("productImage") productImage: any;
+  constructor(
+    private productService: ProductService,
+    private afStorage: AngularFireStorage,
+    ) { }
 
   ngOnInit(): void {
     console.log("starting");
@@ -33,20 +38,47 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  changeImage(p: any) {
+    console.log(document.getElementById(p.key + "-image")?.click());
+  }
+
+  addImage(e: any, p: any) {
+    let selectedFile = e.target.files.item(0);
+    let path = '/users/' + Date.now() + `_${selectedFile.name}`;
+    let ref = this.afStorage.ref(path)
+    ref.put(selectedFile).then(() => {
+      ref.getDownloadURL().subscribe((data: any) => {
+        p.photoURL = data;
+      });
+    });
+  }
+
   addProduct() {
     this.titleElement.nativeElement.classList.remove("bg-danger");
     this.descriptionElement.nativeElement.classList.remove("bg-danger");
     this.priceElement.nativeElement.classList.remove("bg-danger"); 
     this.quantityElement.nativeElement.classList.remove("bg-danger");
+    
     if (this.newProduct.title != "" && this.newProduct.description != "" && this.newProduct.price != null && this.newProduct.quantity != null) {
-      this.newProduct.dateTime = new Date().valueOf();
-      this.productService.createProduct(this.newProduct);
-      this.newProduct = {
-        title: "",
-        description: "",
-        price: null,
-        quantiy: null,
-      };
+      let selectedFile = this.productImage.nativeElement.files.item(0);
+      let path = '/users/' + Date.now() + `_${selectedFile.name}`;
+      let ref = this.afStorage.ref(path)
+      ref.put(selectedFile).then(() => {
+        ref.getDownloadURL().subscribe((data: any) => {
+          this.newProduct.photoURL = data;
+          this.newProduct.dateTime = new Date().valueOf();
+          this.productService.createProduct(this.newProduct);
+          this.newProduct = {
+            title: "",
+            description: "",
+            price: null,
+            quantiy: null,
+          };
+        });
+      }).catch((err: Error) => {
+        console.log(err)
+      });
+
     } else {
       if (this.newProduct.title == "") this.titleElement.nativeElement.classList.add("bg-danger");
       if (this.newProduct.description == "") this.descriptionElement.nativeElement.classList.add("bg-danger");
