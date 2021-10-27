af09805b503c10be89613d8a2824ce72ff53e3c3
matfei_nikolaev <matfeinikolaev@gmail.com>
2021-10-27 11:23:26 -0500

    Set up the check of the required fields

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from '../services/product.service.ts.service';
 import { take } from 'rxjs/operators';
 @Component({
@@ -8,7 +8,16 @@ import { take } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
   products: Array<any> = [];
-  newProduct: any = {};
+  newProduct: any = {
+    title: "",
+    description: "",
+    price: null,
+    quantiy: null,
+  };
+  @ViewChild("titleElement") titleElement: any;
+  @ViewChild("descriptionElement") descriptionElement: any;
+  @ViewChild("priceElement") priceElement: any;
+  @ViewChild("quantityElement") quantityElement: any;
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
@@ -25,9 +34,25 @@ export class HomeComponent implements OnInit {
   }
 
   addProduct() {
-    this.newProduct.dateTime = new Date().valueOf();
-    this.productService.createProduct(this.newProduct);
-    this.newProduct = {};
+    this.titleElement.nativeElement.classList.remove("bg-danger");
+    this.descriptionElement.nativeElement.classList.remove("bg-danger");
+    this.priceElement.nativeElement.classList.remove("bg-danger"); 
+    this.quantityElement.nativeElement.classList.remove("bg-danger");
+    if (this.newProduct.title != "" && this.newProduct.description != "" && this.newProduct.price != null && this.newProduct.quantity != null) {
+      this.newProduct.dateTime = new Date().valueOf();
+      this.productService.createProduct(this.newProduct);
+      this.newProduct = {
+        title: "",
+        description: "",
+        price: null,
+        quantiy: null,
+      };
+    } else {
+      if (this.newProduct.title == "") this.titleElement.nativeElement.classList.add("bg-danger");
+      if (this.newProduct.description == "") this.descriptionElement.nativeElement.classList.add("bg-danger");
+      if (this.newProduct.price == null) this.priceElement.nativeElement.classList.add("bg-danger");
+      if (this.newProduct.quantity == null) this.quantityElement.nativeElement.classList.add("bg-danger");
+    }
   }
 
   deleteProduct(key: any) {
